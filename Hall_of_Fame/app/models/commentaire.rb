1cbc2de834a1e54695c05db2ef26292007d83490
washigeri <malek.elouerghi@gmail.com>
2016-05-03 16:19:30 +0200

    modif models

--- Hall_of_Fame/app/models/commentaire.rb
+++ Hall_of_Fame/app/models/commentaire.rb
@@ -1,2 +1,7 @@
 class Commentaire < ActiveRecord::Base
+	validates: id_commentaire, presence: true
+	validates: id_post, presence: true
+	validates: id_auteur, presence: true
+	validates: date, presence: true
+	validates: contenu, presence: true
 end
--- Hall_of_Fame/app/models/user.rb
+++ Hall_of_Fame/app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+	validates: id_user, presence: true
+	validates: pseudo, presence: true
+	validates: password, presence: true
+	validates: nb_posts, presence: true
+	validates: nom, presence: true
+	validates: prenom, presence: true
+	validates: email, presence: true
+	validates: date_inscription, presence: true
 end
